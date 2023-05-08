455228b986d5c60757e12ec7115f068df69cfd3e
MisakiTaguchi <128069219+Yukihira77@users.noreply.github.com>
2023-05-08 23:32:14 +0000

    [finish]応用６

--- bookers2-debug/app/controllers/book_comments_controller.rb
+++ bookers2-debug/app/controllers/book_comments_controller.rb
@@ -2,14 +2,18 @@ class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comment.new(book_comment_params)
-    comment.book.id = book.id
+    comment.book_id = book.id
     comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    #redirect_to request.referer
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    resirect_to request.referer
+    @book = Book.find(params[:book.id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    #redirect_to request.referer
   end
 
   private
